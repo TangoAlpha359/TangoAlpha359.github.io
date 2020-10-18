2810cb6665a443ea2aeb12093134013fe7e23345
TangoAlpha's Git <andrewterrell.nec@gmail.com>
2020-10-18 15:55:01 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/tangoalpha359/tangoalpha359.github.io";
+const repoURL = "https://github.com/tangoalpha359/tangoalpha359.github.io.git";
 
 ghpages.publish(
   pathname,
